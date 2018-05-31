9cf5202cad644ec2444def089f9d91c6edd50845
fibremint <fibremint@gmail.com>
2018-05-31 23:34:55 +0900

    add new result type

--- src/main/java/com/fibremint/blockchain/message/model/MessageResult.java
+++ src/main/java/com/fibremint/blockchain/message/model/MessageResult.java
@@ -4,15 +4,23 @@ import com.google.gson.annotations.SerializedName;
 
 public class MessageResult extends MessageBase {
     private Type result;
+    private String description;
 
     public MessageResult(Type result) {
         super(MessageType.result);
         this.result = result;
     }
 
+    public MessageResult(Type result, String description) {
+        super(MessageType.result);
+        this.result = result;
+        this.description = description;
+    }
+
     public enum Type {
         @SerializedName("accepted") accepted,
-        @SerializedName("denied") denied
+        @SerializedName("denied") denied,
+        @SerializedName("error") error
     }
 
 }
