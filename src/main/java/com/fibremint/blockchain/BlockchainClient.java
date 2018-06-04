13946afc735c24c91776f26883ac1646ac53dd79
fibremint <fibremint@gmail.com>
2018-06-04 16:46:13 +0900

    base code

--- src/main/java/com/fibremint/blockchain/BlockchainClient.java
+++ src/main/java/com/fibremint/blockchain/BlockchainClient.java
@@ -1,4 +1,11 @@
 package com.fibremint.blockchain;
 
+import com.fibremint.blockchain.blockchain.Wallet;
+
 public class BlockchainClient {
+    private Wallet wallet;
+
+    public static void main(String[] arsg) {
+
+    }
 }
