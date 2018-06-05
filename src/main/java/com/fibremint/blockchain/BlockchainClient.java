ccdacaa6b1ffa860becf8e70a55970c1f4d9b49f
fibremint <fibremint@gmail.com>
2018-06-05 13:49:14 +0900

    in development commit

---
+++ src/main/java/com/fibremint/blockchain/BlockchainClient.java
@@ -0,0 +1,11 @@
+package com.fibremint.blockchain;
+
+import com.fibremint.blockchain.blockchain.Block;
+
+public class BlockchainClient {
+    private Block lastBlock;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/fibremint/blockchain/BlockchainMiner.java
@@ -0,0 +1,18 @@
+package com.fibremint.blockchain;
+
+import com.fibremint.blockchain.blockchain.Wallet;
+
+import javax.swing.*;
+
+public class BlockchainMiner extends JFrame implements Runnable {
+    private Wallet wallet;
+
+    @Override
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
